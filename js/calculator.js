c1e1cdf5ec02e50f39c2ff1e24bef6cdfe5f6efa
rahib <rsayed@spartaglobal.com>
2019-01-29 08:46:00 +0000

    calculator

---
+++ js/calculator.js
@@ -0,0 +1,12 @@
+var num1;
+var num2;
+var operator;
+num1 = prompt("Please enter first number");
+
+operator = prompt("Please enter operator");
+
+num2 = prompt("Please enter second number");
+
+var num3 = eval(num1 + operator + num2);
+
+alert ("Your answer is " + num3);
